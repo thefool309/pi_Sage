0fd246ffc545c3ba104e60a1ff47aed15cec23d2
thefool309 <themagician309@gmail.com>
2025-03-22 18:38:11 -0500

    Finished an example model for the database in the backend code. This may be used later as the User model.

--- backend/src/db/models/ExampleUser.ts
+++ backend/src/db/models/ExampleUser.ts
@@ -1,5 +1,6 @@
-import { DataTypes, Model } from "sequelize";
+import { Model, DataTypes } from 'sequelize';
 import { sequelize } from '../database'
+import { toNamespacedPath } from 'path';
 
 
 // Define a TypeScript interface for type-checking.
@@ -9,3 +10,33 @@ export interface IUser {
     email: string;
 }
 
+// Extend Sequelize's Model class.
+export class User extends Model<IUser> implements IUser {
+    public id!: number;       // The "!" tells TypeScript that this value will be set.
+    public name!: string;
+    public email!: string;
+  }
+
+// Initialize the model and define its schema.
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER.UNSIGNED, // An unsigned integer.
+        autoIncrement: true,                // Automatically increments.
+        primaryKey: true,                   // Primary key of the table.
+      },
+      name: {
+        type: new DataTypes.STRING(128),    // String with a max length of 128.
+        allowNull: false,                   // Cannot be null.
+      },
+      email: {
+        type: new DataTypes.STRING(128),
+        allowNull: false,
+        unique: true,                       // Must be unique.
+      },
+    },
+    {
+      tableName: 'users',   // Name of the table in the database.
+      sequelize,            // Passing the Sequelize instance.
+    }
+  );
