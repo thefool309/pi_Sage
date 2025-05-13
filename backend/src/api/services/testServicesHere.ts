a4019ff318d621e207be2573c507b6f1dc1dda2d
thefool309 <themagician309@gmail.com>
2025-05-13 12:54:42 -0500

    added a file for unit testing

--- backend/src/api/services/testServicesHere.ts
+++ backend/src/api/services/testServicesHere.ts
@@ -1,14 +1,17 @@
+import assert from "assert";
 import { parseAndSaveScan } from "./scanDbService";
 
 export function TestParseAndSaveScanVsExpectedOutput() {
     
     let data = parseAndSaveScan("./test-output.xml");
     if (data != null) {
-        console.log("Data parse success!");
+        console.log("Data parse success! Test passed!");
         console.log("Please check the data and make sure it is correct.");
         console.log(JSON.stringify(parseAndSaveScan("./test-output.xml")));
     }
     else {
+        //assert.notEqual(null);
         console.error("You suck at coding. parseAndSaveScan returned null");
     }
+    
 }
