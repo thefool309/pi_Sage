90ea0d5b44cc9179ece96fa7d7a7525deb8b4655
thefool309 <themagician309@gmail.com>
2025-05-15 18:26:49 -0500

    expanded Port db Model to match the given json data. Also got extensions and prettier working again

--- backend/src/api/services/nmapScanner.ts
+++ backend/src/api/services/nmapScanner.ts
@@ -1,49 +1,50 @@
-import { spawn } from 'child_process';
-import fs from 'fs/promises';
-import { parseStringPromise } from 'xml2js';
-
+import { spawn } from "child_process";
+import fs from "fs/promises";
+import { parseStringPromise } from "xml2js";
 
 export async function runNmap(target: string | undefined): Promise<string> {
-    return new Promise((resolve, reject) => {
-        if(target == undefined){
-            return reject(`Target is undefined`);           //error checking in case target is undefined
-        }
-        const command = 'nmap';
-        const outputPath = './src/api/services/.nmap-output/output.xml'
-        // Add -vv for more verbosity if needed
-        // -T4 is for speed -v is for verbosity -sV detects the version of services running on open ports
-        // -F is for fast, and -oX outputs to an xml file
-        const args = ['-T4', '-v', '-sV', '-F', '-oX', outputPath, target];
-    
-        const nmapProcess = spawn(command, args);
-    
-        let output = '';
-        nmapProcess.stdout.on('data', (data) => {       //successful output
-          output += data.toString();
-          console.log(data.toString());
-        });
-    
-        nmapProcess.stderr.on('data', (data) => {       //stderr won't halt the process but will print an error to the console for debugging
-          console.error(`Nmap stderr: ${data}`);
-        });
-    
-        nmapProcess.on('error', (error) => {            // on an error we send a rejection carrying the error message. 
-          reject(`Error: ${error.message}`);
-        });
-    
-        nmapProcess.on('close', async (code) => {             //if nmap exited with an error code reject with that code. 
-          if (code !== 0) {
-            return reject(`Nmap process exited with code ${code}`);
-          }
-          try {
-            const xmlData = await fs.readFile(outputPath, 'utf-8'); // convert xml file to string
-            var jsonData = await parseStringPromise(xmlData);
-            resolve(JSON.stringify(jsonData));      // parse into string for the backend to handle
-          } 
-          catch (err) {
-            reject(`Failed to read or parse JSON ouput: ${err}`);
-          }
-          
-        });
+  return new Promise((resolve, reject) => {
+    if (target == undefined) {
+      return reject(`Target is undefined`); //error checking in case target is undefined
+    }
+    const command = "nmap";
+    const outputPath = "./src/api/services/.nmap-output/output.xml";
+    // Add -vv for more verbosity if needed
+    // -T4 is for speed -v is for verbosity -sV detects the version of services running on open ports
+    // -F is for fast, and -oX outputs to an xml file
+    const args = ["-T4", "-v", "-sV", "-F", "-oX", outputPath, target];
+
+    const nmapProcess = spawn(command, args);
+
+    let output = "";
+    nmapProcess.stdout.on("data", (data) => {
+      //successful output
+      output += data.toString();
+      console.log(data.toString());
     });
+
+    nmapProcess.stderr.on("data", (data) => {
+      //stderr won't halt the process but will print an error to the console for debugging
+      console.error(`Nmap stderr: ${data}`);
+    });
+
+    nmapProcess.on("error", (error) => {
+      // on an error we send a rejection carrying the error message.
+      reject(`Error: ${error.message}`);
+    });
+
+    nmapProcess.on("close", async (code) => {
+      //if nmap exited with an error code reject with that code.
+      if (code !== 0) {
+        return reject(`Nmap process exited with code ${code}`);
+      }
+      try {
+        const xmlData = await fs.readFile(outputPath, "utf-8"); // convert xml file to string
+        var jsonData = await parseStringPromise(xmlData);
+        resolve(JSON.stringify(jsonData)); // parse into string for the backend to handle
+      } catch (err) {
+        reject(`Failed to read or parse JSON ouput: ${err}`);
+      }
+    });
+  });
 }
--- backend/src/db/models/Port.ts
+++ backend/src/db/models/Port.ts
@@ -1,67 +1,102 @@
-import { Model, DataTypes } from 'sequelize';
-import { sequelize } from '../database';
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../database";
 
 export interface IPort {
-    id: number;
-    portNumber: number;
-    protocol: string;
-    state: string;
-    serviceName?: string;
-    hostId: number; 
-    extraInfo?: string;
+  id: number;
+  portNumber: number;
+  protocol: string;
+  //state members
+  state: string;
+  reason: string;
+  reason_ttl: string;
+  //service members
+  service_name: string;
+  service_product?: string;
+  service_version?: string;
+  service_method?: string;
+  service_confidence?: string;
+  cpe?: string;
 }
 
 export class Port extends Model<IPort> implements IPort {
-    declare id: number;
-    declare portNumber: number;
-    declare protocol: string;
-    declare state: string;
-    declare serviceName: string | undefined;
-    declare hostId: number;
-    declare extraInfo: string | undefined;
+  declare id: number;
+  // top level members
+  // inside an object named '$' inside the port object in the json
+  declare portNumber: number;
+  declare protocol: string;
+  //state members
+  declare state: string;
+  declare reason: string;
+  declare reason_ttl: string;
+  //service members
+  declare service_name: string;
+  declare service_product?: string;
+  declare service_version?: string;
+  declare service_method?: string;
+  declare service_confidence?: string;
+  declare cpe?: string;
 }
 
 Port.init(
-    {
-        id: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        portNumber: {
-            allowNull: false,
-            type: DataTypes.INTEGER.UNSIGNED
-        },
-        protocol: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        state: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        hostId: {
-            type: DataTypes.INTEGER.UNSIGNED,
-            allowNull: false
-        },
-        serviceName: {
-            type: DataTypes.STRING,
-            allowNull: true
-        },
-        extraInfo: {
-            type: DataTypes.STRING,
-            allowNull:true
-        }
-        
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        tableName: 'ports',
-        indexes: [
-            {
-              unique: true,
-              fields: ['hostId', 'portNumber']
-            }
-          ]
-    }
-)
+    portNumber: {
+      allowNull: false,
+      type: DataTypes.INTEGER.UNSIGNED,
+    },
+    protocol: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reason: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    reason_ttl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    service_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    service_product: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    service_version: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    service_method: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    service_confidence: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    cpe: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    tableName: "ports",
+    indexes: [
+      {
+        unique: true,
+        fields: ["hostId", "portNumber"],
+      },
+    ],
+  }
+);
