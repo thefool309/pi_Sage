5869060fdde3ce8ed165fbf6cdef77b595daf339
thefool309 <themagician309@gmail.com>
2025-05-22 14:57:20 -0500

    added TODOs and changed the name of scanRoute.ts to scanRoutes.ts to ensure consistent naming conventions

---
+++ backend/src/api/routes/dbRoutes.ts
@@ -0,0 +1,5 @@
+// TODO: implement router for '/data' route
+
+// TODO: Implement api route to query the database for the most recent scan
+
+// TODO: Implement api route to query the database for the most recent scans within a displayCount range. This will be for the "Scan History" screen.
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,16 +1,16 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 const result = dotenv.config();
 if (result.error) {
-  console.error('Error loading .env file:', result.error);
+  console.error("Error loading .env file:", result.error);
 }
-console.log('Parsed .env variables:', result.parsed);
+console.log("Parsed .env variables:", result.parsed);
 
-import express, { Request, Response } from 'express';
-import { connectDB, sequelize } from './db/database';
-import cors from 'cors';
-import { User } from './db/models/ExampleUser'; //import models
+import express, { Request, Response } from "express";
+import { connectDB, sequelize } from "./db/database";
+import cors from "cors";
+import { User } from "./db/models/ExampleUser"; //import models
 
-import nMapRouter from './api/routes/scanRoute';
+import nMapRouter from "./api/routes/scanRoutes";
 
 const app = express();
 
@@ -22,9 +22,9 @@ const PORT = process.env.PORT || 3000;
 
 connectDB().then(async () => {
   try {
-    console.log('All models were synchronized successfully.\n');
-  } catch(syncError) {
-    console.error('Error synchronizing models: ', syncError, '\n');
+    console.log("All models were synchronized successfully.\n");
+  } catch (syncError) {
+    console.error("Error synchronizing models: ", syncError, "\n");
   }
 });
 
@@ -32,12 +32,15 @@ app.get("/status", (request: Request, response: Response) => {
   response.send("Server is active and running...\n");
 });
 
-app.use('/scan', nMapRouter);   // here we map the nMapRouter to /scan in the root project
-                                // so we can access it's functionality via insert-host-name:3000/scan
-app.get('/', (req:Request, res: Response) => {
-  res.send('Welcome to the pi_Shield API');
-})
+app.use("/scan", nMapRouter); // here we map the nMapRouter to /scan in the root project
+// so we can access it's functionality via <insert-host-name>:3000/scan
+
+// TODO: map the dbRouter to '/data' so we can expose the endpoints mapped to the dbRouter
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the pi_Shield API");
+});
 
 app.listen(PORT, () => {
-  console.log("Server Listening on PORT:", PORT, '\n');
+  console.log("Server Listening on PORT:", PORT, "\n");
 });
